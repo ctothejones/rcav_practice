f5aa5fdb69ee3e91bf5bcf15c3ef636ed597e7e1
Colin Jones <colinsjones11@gmail.com>
2014-05-11 11:10:13 -0500

    Fixed Routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,8 @@ Rails.application.routes.draw do
   get("/", { :controller => "calculations", :action => "home" })
 
   get("/square", { :controller => "calculations", :action => "square"})
-  get("/square/:number", { :controller => "calculations", :action => "square"})
 
   get("/square_root", { :controller => "calculations", :action => "sqrt" })
-  get("/square_root/:number", { :controller => "calculations", :action => "sqrt" })
 
   get("/payment", { :controller => "calculations", :action => "pmt" })
-  get("/payment/:interest_rate/:number_of_payments/:present_value", { :controller => "calculations", :action => "pmt" })
 end
